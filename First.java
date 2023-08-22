438d240e94677aaa67da2ec29a02b9e25fdf8a93
Mohammad Kashif <139605675+kashif-kairo@users.noreply.github.com>
2023-08-22 12:29:32 +0530

    Update First.java

--- First.java
+++ First.java
@@ -19,5 +19,25 @@ public class First {
         System.out.println("Enter the char");
         char a = st.next().charAt(0);
         System.out.println(a);
+        Scanner sk = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String c = sk.next(); // next() will not all line if line has a space
+        System.out.println(c);
+        Scanner sv = new Scanner(System.in);
+        System.out.println("Enter the Line");
+        String d = sk.nextLine();
+        System.out.println(d); //nextLine() will take whole line
+        
+        /** Exwecise 1
+         write a java program to read integer value and print its equivalent character value **/
+        
+        Scanner sp = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int N = sp.nextInt();
+        
+        char C =(char)N;
+        System.out.println("sample output ");
+        System.out.println(C);
+        
     }
 }
