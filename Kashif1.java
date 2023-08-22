577a1c1a5e67cbacc37430c7f2c56b9acb7b18e8
Mohammad Kashif <139605675+kashif-kairo@users.noreply.github.com>
2023-08-22 10:57:41 +0530

    Add files via upload

---
+++ Kashif1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kashif1;
+
+/**
+ *
+ * @author E-LAB-PC53
+ */
+public class Kashif1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Kashif ");
+    }
+}
