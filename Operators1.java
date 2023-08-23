cac59ba207b80e7e10ffa99e029dc827e05ded7f
Mohammad Kashif <139605675+kashif-kairo@users.noreply.github.com>
2023-08-23 12:26:08 +0530

    Add files via upload

---
+++ Operators1.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kashif1;
+
+/**
+ *
+ * @author E-LAB-PC53
+ */
+import java.util.Scanner;
+public class Operators1 {
+     public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+//         [{(  Arthmetic Operation of two numbers )}]
+
+//        System.out.println("Enter value a");
+//        int a = sc.nextInt();
+//        System.out.println("Enter value b");
+//        int b = sc.nextInt();
+//        System.out.println("performing addition");
+//        System.out.println(a+b);
+//        System.out.println("performing Substraction");
+//        System.out.println(a-b);
+//        System.out.println("performing Multiplication");
+//        System.out.println(a*b);
+//        System.out.println("performing Division quaotiant");
+//        System.out.println(a/b);
+//        System.out.println("performing Remainder");
+//        System.out.println(a%b);
+
+//--------------------------------------------------     
+//         [{( Increment/Decrement Operator )}]
+//        System.out.println("Enter the value of a ");
+//        
+//        int a = sc.nextInt();
+//        System.out.println("Pre increment");
+//        System.out.println(++a);
+//        
+//        System.out.println("post increment");
+//        System.out.println(a++);
+//        System.out.println(a);
+        
+
+//----------------------------------------------------------------
+
+//         [{( Shift Operator )}]
+//        int a =7;
+//        int b= 1;
+//        int c= 2;
+//        System.out.println("1 byte shift");
+//        System.out.println(a>>b);
+//        System.out.println(a<<b);
+//        System.out.println("2 byte shift");
+//        System.out.println(a>>c);
+//        System.out.println(a<<c);
+//        
+//-------------------------------------------------------------------------
+
+//         [{( Relational Operation )}]
+//        int a =5;
+//        int b= 6;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println("==");
+//        System.out.println(a==b);
+//        System.out.println("!=");
+//        System.out.println(a!=b);
+//        System.out.println(">");
+//        System.out.println(a>b);
+//        System.out.println("<");
+//        System.out.println(a<b);
+//        System.out.println(">=");
+//        System.out.println(a>=b);
+//        System.out.println("<=");
+//        System.out.println(a<=b);
+
+        
+//---------------------------------------------------------------------
+
+//         [{( Bitwise Operation )}]
+//            int a =5;
+//            int b= 6;
+//            System.out.println(a);
+//            System.out.println(b);
+//            System.out.println("A or(|) B");
+//            System.out.println(a|b);
+//            System.out.println("A and(&) B");
+//            System.out.println(a&b);           
+//            System.out.println("A XOR(^) B");
+//            System.out.println(a^b);
+//            System.out.println("Complement (~) B");
+//            System.out.println( ~b);
+//       
+//---------------------------------------------------------------------
+
+//         [{( Logical Operation )}]
+
+        boolean a= true;
+        boolean b= false;
+        System.out.println(a&&b);
+        System.out.println(a||b);
+        System.out.println(!b);
+        System.out.println(a^b);
+        
+
+
+
+        
+    }
+    
+}
