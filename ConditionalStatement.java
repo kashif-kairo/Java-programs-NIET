430affcad0ecb762b25362ab64012ce0cc121a08
Mohammad Kashif <139605675+kashif-kairo@users.noreply.github.com>
2023-08-24 12:26:42 +0530

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kashif1;
+
+/**
+ *
+ * @author E-LAB-PC53
+ */
+import java.util.Scanner;
+public class ConditionalStatement {
+ /**  Problem
+Given an integer, n, perform the following conditional actions:
+
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Input Format:
+A single line containing a positive integer, n.
+Constraints:
+n lies between 1 and 100
+
+Input:
+17
+
+Output:
+Weird*/
+//    public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the number ");
+//        int n = sc.nextInt();
+//        if(n%2==1){
+//            System.out.print("Weird");
+//        }
+//        else if((n%2==0)&&(n>=2&&n<=5)){
+//                System.out.print("Not Weird");
+//                    }
+//        else if((n%2==0)&&(n>=6&&n<=20)){
+//              System.out.print("Weird");
+//                    }
+//        else{
+//            System.out.print("Not Weird");
+//        }
+//        
+//----------------------------------------------------------------------------------------
+
+//java program to check greatest number
+//        public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the Numbers");
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//        int o= sc.nextInt();
+//        if(n>m){
+//            if(n>o){
+//                System.out.print((n)+" is greatest");
+//            }
+//            else{
+//                System.out.print((0)+" is greatest");
+//                
+//            }
+//        }
+//        else{
+//            if(m>o){
+//                System.out.print((m)+" is greatest");
+//            }
+//            else{
+//                System.out.print((o)+" is greatest");
+//                
+//            }
+//        }
+//        }
+//
+//        }
+//-------------------------------------------------------------------------------------------
+//   Switch Case
+//        public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the Numbers ");
+//        int day = sc.nextInt();
+//            switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                 System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Invalid Input");
+//                
+//        }
+//}
+    //----------------------------------------------------------------------------------
+    
+    // do while
+//       public static void main(String[] args){
+//       Scanner sc = new Scanner(System.in);
+//       System.out.print("Enter the Numbers");
+//       int n =sc.nextInt();
+//       int m=n;
+//       int sum=0;
+////       -----------------------------------
+// count the number of digit
+//       int count =0;
+//       do{
+//           ++count;
+//           n=n/10;
+//       }while(n!=0);
+//       System.out.print(count);       
+//       }
+//-----------------------------------------------------
+
+// reverse the number
+//        while(m>0){
+//        int r = m%10;
+//        sum=sum*10+r;
+//        m=m/10;
+//        }
+//        System.out.print(sum);
+//       
+       
+       
+       
+}
+}
+        
+        
+        
+    
+    
+
