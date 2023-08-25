c7c5b5c73dc8b8098ff27e68c6f3a1f32cfa004e
Mohammad Kashif <139605675+kashif-kairo@users.noreply.github.com>
2023-08-25 11:17:55 +0530

    Add files via upload

---
+++ Conditional2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kashif1;
+
+/**
+ *
+ * @author E-LAB-PC53
+ */
+public class Conditional2 {
+    public static void main(String[] args){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<2;j++){
+                    System.out.print((i+1)+" ");}
+        System.out.println();
+        }
+            
+    
+    
+    for(int k=1;k<=3;k++){
+        if(k==2){
+//            continue;
+            break;
+        }
+        else{
+        System.out.println("Assalam u alaikum Duniya");
+}
+    
+}
+}
+}
